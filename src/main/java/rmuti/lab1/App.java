47fe8098cecad7956b41e5b4c78cda75287ea46b
boss959595 <siwanutboss@gmail.com>
2017-01-12 19:38:26 +0700

    Hello git

---
+++ src/main/java/rmuti/lab1/App.java
@@ -0,0 +1,21 @@
+package rmuti.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ArrayOperation obj = new ArrayOperation();
+        
+        int sumObj = obj.sumArrays();
+        System.out.println("Sum = "+sumObj);
+        
+        int maxObj = obj.maxArrays();
+        System.out.println("Max Number = "+maxObj);
+
+        
+    }
+}
